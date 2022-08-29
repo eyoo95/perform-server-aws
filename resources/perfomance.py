b301afe2c69d02ea7fd915ea3b9ac5e431b9132e
eyoo95 <105832377+eyoo95@users.noreply.github.com>
2022-08-29 15:34:37 +0900

    add

--- resources/perfomance.py
+++ resources/perfomance.py
@@ -100,7 +100,7 @@ class PerformanceSearchResource(Resource):
 
 #         # 파라미터에 들어갈 정보
 #         params = { "service" : Config.KOPIS_ACCESS_KEY }
-        
+            
 #         prfIdList = []
 #         for i in range(len(resultList)):
 #             prfIdList.append(resultList[i]['mt10id'])
@@ -296,11 +296,9 @@ class PerformancePlaceSearchResource(Resource):
         }
 
         plcIdList = []
-        i = 0
         for i in range(len(resultList)):
             plcIdList.append(resultList[i]['mt10id'])
-        
-        i = 0
+
         for i in plcIdList:
 
             # 요청하는 API의 URL과 API에서 요구하는 데이터 입력
@@ -416,8 +414,6 @@ class NearByPerformanceResource(Resource) :
         # 공연과 시설의 정보 저장
         try : 
             performanceList = []
-
-            i = 0
             for i in range(len(resultList)) :
             # 파라미터에 들어갈 정보
                 params = { "service" : Config.KOPIS_ACCESS_KEY }
@@ -437,7 +433,6 @@ class NearByPerformanceResource(Resource) :
         # 공연 시설의 경도 위도를 저장
         performanceDetailList = []
         try :
-            i = 0
             for i in range(len(performanceList)) :
                 # 파라미터에 들어갈 정보
                 params = { "service" : Config.KOPIS_ACCESS_KEY }
@@ -457,7 +452,6 @@ class NearByPerformanceResource(Resource) :
         # 내 주위 5km에 상영중인 시설 위치 찾기
         nearByPlaceList = []
         try :
-            i = 0
             for i in range(len(performanceDetailList)) :
                 # 파라미터에 들어갈 정보
                 params = {  "key" : Config.GOOGLE_API_KEY,
